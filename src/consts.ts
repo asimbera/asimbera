653e39afe5c9115fdd94fd1c5b683e3201aa69a8
Asim Bera <asimbera@outlook.in>
2023-02-03 16:16:19 +0530

    patch: update site description

--- src/consts.ts
+++ src/consts.ts
@@ -1,5 +1,2 @@
-// Place any global data in this file.
-// You can import this data from anywhere in your site by using the `import` keyword.
-
 export const SITE_TITLE = 'Asim\'s Blog';
-export const SITE_DESCRIPTION = 'Welcome to my website!';
+export const SITE_DESCRIPTION = 'An open journal on programming, infosec, design and much more...';
