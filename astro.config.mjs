24e082d4e3dd66ac0a260b25a2e2ac3174e6a06a
Asim Bera <asimbera@outlook.in>
2025-03-04 20:22:32 +0530

    fix: remove backticks from inline code and change theme

--- astro.config.mjs
+++ astro.config.mjs
@@ -13,7 +13,7 @@ export default defineConfig({
   integrations: [
     sitemap(),
     expressiveCode({
-      themes: ['monokai'],
+      themes: ['one-dark-pro'],
       styleOverrides: {
         codeFontFamily: '"Iosevka", monospace',
       },
